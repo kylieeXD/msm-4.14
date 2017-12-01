f17a9347e439695840d7941950b724d018956964
Al Viro <viro@zeniv.linux.org.uk>
2017-12-01 17:12:45 -0500

    new primitive: vfs_mkobj()
    Similar to vfs_create(), but with caller-supplied callback (and
    argument for it) to be used instead of ->create().
    
    Change-Id: Ie67e2af6fb43bf75d316105e6eb5c14fc3663dd4
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1685,6 +1685,10 @@ extern long compat_ptr_ioctl(struct file *file, unsigned int cmd,
 #define compat_ptr_ioctl NULL
 #endif
 
+int vfs_mkobj(struct dentry *, umode_t,
+		int (*f)(struct dentry *, umode_t, void *),
+		void *);
+
 /*
  * VFS file helper functions.
  */
