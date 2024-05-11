03728367d0f1a06bfc7b63c35d2c1189ff7444c7
Sultan Alsawaf <sultan@kerneltoast.com>
2024-05-11 00:10:43 -0700

    sched/core: Forbid Unity-based games from changing their CPU affinity
    Unity-based games (such as Wild Rift) like to shoot themselves in the foot
    by setting a nonsense CPU affinity, restricting the game to a narrow set of
    CPU cores that it thinks are the "big" cores in a heterogeneous CPU. It
    assumes that CPUs only have two performance domains (clusters), and
    therefore royally mucks up games' CPU affinities on CPUs which have more
    than two performance domains.
    
    Check if a setaffinity target task is part of a Unity-based game and
    silently ignore the setaffinity request so that it can't sabotage itself.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5927,7 +5927,7 @@ long sched_setaffinity(pid_t pid, const struct cpumask *in_mask)
 {
 	cpumask_var_t cpus_allowed, new_mask;
 	struct task_struct *p;
-	int retval;
+	int retval = 0;
 	int dest_cpu;
 	cpumask_t allowed_mask;
 
