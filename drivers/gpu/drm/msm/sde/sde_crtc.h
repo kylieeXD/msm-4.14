118b3104fcc872316f4381dbc8ead0f43ee937bb
kylieeXD <khayloaftck@gmail.com>
2025-12-10 18:02:38 +0000

    drivers: Add MIUI display support

--- drivers/gpu/drm/msm/sde/sde_crtc.h
+++ drivers/gpu/drm/msm/sde/sde_crtc.h
@@ -292,6 +292,7 @@ struct sde_crtc {
 	u32 sbuf_rot_id_old;
 	u32 sbuf_rot_id_delta;
 	struct kthread_delayed_work idle_notify_work;
+	struct kthread_work early_wakeup_work;
 
 	struct sde_power_event *power_event;
 
